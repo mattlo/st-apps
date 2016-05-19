8824d02a4041c586ce40df54aea064485126df5b
Matt Lo <mattlo.developer@gmail.com>
2016-05-19 01:00:02 -0500

    added additional controls to manage door

--- motion-door-off.groovy
+++ motion-door-off.groovy
@@ -1,20 +1,22 @@
 definition(
-  name: "Light off by motion conditional",
+  name: "Automatic lights by motion+contact",
   namespace: "mattlo",
   author: "Matt Lo",
-  description: "Turn off lights when theres no motion and a door is open",
+  description: "Manage lights based on a motion sensor and contact sensor",
   category: "Convenience",
+  iconUrl: "https://www.gravatar.com/avatar/26b312fb15048201616ab0c2115f8ebc?s=256&d=identicon&r=PG",
+  iconX2Url: "https://www.gravatar.com/avatar/26b312fb15048201616ab0c2115f8ebc?s=256&d=identicon&r=PG"
 )
 
 preferences {
-  section("Turn off when motion is detected"){
+  section("Manage lights with motion detector"){
     input "motionSensor", "capability.motionSensor", title: "Which Motion Sensor?"
   }
-  section("And when the door sensor is closed"){
-    input "doorControl", "capability.doorControl", title: "Which Door?"
+  section("And manage with a multi-purpose contact sensor"){
+    input "contactSensor", "capability.contactSensor", title: "Which Door?"
   }
-  section("Turn off lights"){
-    input "lights", "capability.switch", title: "Wich Lights?", multiple: true
+  section("Lights"){
+    input "lights", "capability.switch", title: "which Lights?", multiple: true
   }
 }
 
@@ -22,6 +24,7 @@ preferences {
 def installed()
 {
   subscribe(motionSensor, "motion", motionHandler)
+  subscribe(contactSensor, "contact", contactHandler)
 }
 
 def updated()
@@ -34,8 +37,18 @@ def uninstalled() {
   unsubscribe()
 }
 
-def motionHandler(evt) {
-  if (evt.value == "inactive" && doorControl.door === 'open') {
+def contactHandler(evt) {
+  if (evt.value == "closed") {
+    lights.on()
+  } else if(evt.value == "open") {
     lights.off()
+  }
+}
+
+def motionHandler(evt) {
+  if (evt.value == "inactive" && contactSensor.currentValue("contact") == 'open') {
+    lights.off()
+  } else if (evt.value == "active" && contactSensor.currentValue("contact") == 'open') {
+    lights.on()
   }
 }
